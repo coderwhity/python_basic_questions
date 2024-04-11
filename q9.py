2ea745db876b744ada7f95c33f0d4f88926c2aef
Prasad Shinde <132158524+coderwhity@users.noreply.github.com>
2024-04-12 00:57:30 +0530

    Q9 UPDATED

--- q9.py
+++ q9.py
@@ -1,9 +1,5 @@
 # Write a program to make use of conditional operator in Python
 
-n1,n2=45,45
-if(n1>n2):
-    print(f"{n1} is greater then {n2}")
-elif(n1==n2):
-    print(f"{n1} is equal {n2}")
-else:
-    print(f"{n2} is greater then {n1}")
+n1,n2=45,46
+greater = n1 if n1>n2 else n2
+print(f"Greater number is : {greater}")
